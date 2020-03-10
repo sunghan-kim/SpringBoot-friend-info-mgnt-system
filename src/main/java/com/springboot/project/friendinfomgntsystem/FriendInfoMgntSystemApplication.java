18b622151bc445b18026998342c288c3ca2d78e9
shkim4738 <shkim4738@gmail.com>
2020-03-10 22:02:15 +0900

    Init Commit

---
+++ src/main/java/com/springboot/project/friendinfomgntsystem/FriendInfoMgntSystemApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.project.friendinfomgntsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendInfoMgntSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendInfoMgntSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/project/friendinfomgntsystem/FriendInfoMgntSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.project.friendinfomgntsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendInfoMgntSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
