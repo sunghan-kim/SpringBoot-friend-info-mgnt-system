2f7588095c927ad743bf92dd63b34e8eb288c8ee
shkim4738 <shkim4738@gmail.com>
2020-03-11 21:07:50 +0900

    JPA - Lombok - @Getter, @Setter, @ToString

--- src/test/java/com/springboot/project/friendinfomgntsystem/repository/PersonRepositoryTest.java
+++ src/test/java/com/springboot/project/friendinfomgntsystem/repository/PersonRepositoryTest.java
@@ -21,15 +21,18 @@ class PersonRepositoryTest {
 
         person.setName("martin");
         person.setAge(10);
+        person.setBloodType("A");
 
         personRepository.save(person);
 
-//        System.out.println(personRepository.findAll());
+        System.out.println(personRepository.findAll());
+
         List<Person> people = personRepository.findAll();
 
         assertThat(people.size()).isEqualTo(1);
         assertThat(people.get(0).getName()).isEqualTo("martin");
         assertThat(people.get(0).getAge()).isEqualTo(10);
+        assertThat(people.get(0).getBloodType()).isEqualTo("A");
     }
 
 }
