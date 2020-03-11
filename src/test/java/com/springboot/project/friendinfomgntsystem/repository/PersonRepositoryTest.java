a778e1debe7bcbaf45e6fb877d02fefc5036f4dd
shkim4738 <shkim4738@gmail.com>
2020-03-11 20:47:07 +0900

    JPA - Person 객체 데이터 insert, select

---
+++ src/test/java/com/springboot/project/friendinfomgntsystem/repository/PersonRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.springboot.project.friendinfomgntsystem.repository;
+
+import com.springboot.project.friendinfomgntsystem.domain.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PersonRepositoryTest {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Test
+    void crud() {
+        Person person = new Person();
+
+        person.setName("martin");
+        person.setAge(10);
+
+        personRepository.save(person);
+
+//        System.out.println(personRepository.findAll());
+        List<Person> people = personRepository.findAll();
+
+        assertThat(people.size()).isEqualTo(1);
+        assertThat(people.get(0).getName()).isEqualTo("martin");
+        assertThat(people.get(0).getAge()).isEqualTo(10);
+    }
+
+}
