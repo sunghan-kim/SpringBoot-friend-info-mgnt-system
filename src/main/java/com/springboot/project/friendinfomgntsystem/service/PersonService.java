ad57e7b4f672c2a2766700cb3f3f31f076ab5408
shkim4738 <shkim4738@gmail.com>
2020-03-12 22:00:05 +0900

    JPA - Query Method - stream().filter()를 활용한 WHERE 조건 조회

--- src/main/java/com/springboot/project/friendinfomgntsystem/service/PersonService.java
+++ src/main/java/com/springboot/project/friendinfomgntsystem/service/PersonService.java
@@ -1,8 +1,6 @@
 package com.springboot.project.friendinfomgntsystem.service;
 
-import com.springboot.project.friendinfomgntsystem.domain.Block;
 import com.springboot.project.friendinfomgntsystem.domain.Person;
-import com.springboot.project.friendinfomgntsystem.repository.BlockRepository;
 import com.springboot.project.friendinfomgntsystem.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +23,14 @@ public class PersonService {
         return people.stream().filter(person -> person.getBlock() == null).collect(Collectors.toList());
     }
 
+    public List<Person> getPeopleByName(String name) {
+
+        List<Person> people = personRepository.findAll();
+
+        return people.stream().filter(person -> person.getName().equals(name)).collect(Collectors.toList());
+
+    }
+
     @Transactional(readOnly = true)
     public Person getPerson(Long id) {
         Person person = personRepository.findById(id).get();
@@ -33,5 +39,4 @@ public class PersonService {
 
         return person;
     }
-
 }
--- src/test/java/com/springboot/project/friendinfomgntsystem/service/PersonServiceTest.java
+++ src/test/java/com/springboot/project/friendinfomgntsystem/service/PersonServiceTest.java
@@ -33,6 +33,15 @@ class PersonServiceTest {
     }
 
     @Test
+    void getPeopleByName() {
+        givenPeople();
+
+        List<Person> result = personService.getPeopleByName("martin");
+
+        result.forEach(System.out::println);
+    }
+
+    @Test
     void cascadeTest() {
         givenPeople();
 
