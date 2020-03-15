b3654935efc2b6e30e10d0426121d7bfcb81816b
shkim4738 <shkim4738@gmail.com>
2020-03-15 23:01:43 +0900

    Controller - @DeleteMapping - 데이터 삭제

--- src/test/java/com/springboot/project/friendinfomgntsystem/controller/PersonControllerTest.java
+++ src/test/java/com/springboot/project/friendinfomgntsystem/controller/PersonControllerTest.java
@@ -1,5 +1,6 @@
 package com.springboot.project.friendinfomgntsystem.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,10 +21,13 @@ class PersonControllerTest {
 
     private MockMvc mockMvc;
 
+    @BeforeEach // 매번의 테스트마다 해당 메서드가 한 번씩 실행된다.
+    void beforeEach() {
+        mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
+    }
+
     @Test
     void getPerson() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
-
         mockMvc.perform(
                 MockMvcRequestBuilders.get("/api/person/1"))
                 .andDo(print())
@@ -33,8 +37,6 @@ class PersonControllerTest {
 
     @Test
     void postPerson() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
-
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/api/person")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
@@ -51,8 +53,6 @@ class PersonControllerTest {
     @Test
     void modifyPerson() throws Exception {
         // Put 메서드는 항상 Person Entity에 있는 동일한 정보가 들어 있어야 한다. (수정하지 않는 정보까지 필요)
-        mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
-
         mockMvc.perform(
                 MockMvcRequestBuilders.put("/api/person/1")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
@@ -68,11 +68,17 @@ class PersonControllerTest {
 
     @Test
     void modifyName() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
-
         mockMvc.perform(
                 MockMvcRequestBuilders.patch("/api/person/1")
                         .param("name", "martin22"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deletePerson() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete("/api/person/1"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
