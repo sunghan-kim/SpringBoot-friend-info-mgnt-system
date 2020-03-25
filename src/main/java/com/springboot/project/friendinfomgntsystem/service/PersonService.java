90a0dd194884cbd6a35178ae6c3ed4fbe857d1d6
shkim4738 <shkim4738@gmail.com>
2020-03-25 23:32:18 +0900

    Exception - PersonNotFoundException, RenameNotPermittedException

--- src/main/java/com/springboot/project/friendinfomgntsystem/service/PersonService.java
+++ src/main/java/com/springboot/project/friendinfomgntsystem/service/PersonService.java
@@ -2,6 +2,8 @@ package com.springboot.project.friendinfomgntsystem.service;
 
 import com.springboot.project.friendinfomgntsystem.controller.dto.PersonDto;
 import com.springboot.project.friendinfomgntsystem.domain.Person;
+import com.springboot.project.friendinfomgntsystem.exception.PersonNotFoundException;
+import com.springboot.project.friendinfomgntsystem.exception.RenameNotPermittedException;
 import com.springboot.project.friendinfomgntsystem.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,11 +38,11 @@ public class PersonService {
 
     @Transactional
     public void modify(Long id, PersonDto personDto) {
-        Person person = personRepository.findById(id).orElseThrow(() -> new RuntimeException("아이디가 존재하지 않습니다."));
+        Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
         // orElseThrow : 값이 없을 경우 예외를 throw 한다.
 
         if (!person.getName().equals(personDto.getName())) {
-            throw new RuntimeException("이름이 다릅니다.");
+            throw new RenameNotPermittedException();
         }
 
         person.set(personDto);
@@ -50,7 +52,7 @@ public class PersonService {
 
     @Transactional
     public void modify(Long id, String name) {
-        Person person = personRepository.findById(id).orElseThrow(() -> new RuntimeException("아이디가 존재하지 않습니다."));
+        Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
 
         person.setName(name);
 
@@ -59,7 +61,7 @@ public class PersonService {
 
     @Transactional
     public void delete(Long id) {
-        Person person = personRepository.findById(id).orElseThrow(() -> new RuntimeException("아이디가 존재하지 않습니다."));
+        Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
 
         person.setDeleted(true);
 
--- src/test/java/com/springboot/project/friendinfomgntsystem/service/PersonServiceTest.java
+++ src/test/java/com/springboot/project/friendinfomgntsystem/service/PersonServiceTest.java
@@ -3,6 +3,8 @@ package com.springboot.project.friendinfomgntsystem.service;
 import com.springboot.project.friendinfomgntsystem.controller.dto.PersonDto;
 import com.springboot.project.friendinfomgntsystem.domain.Person;
 import com.springboot.project.friendinfomgntsystem.domain.dto.Birthday;
+import com.springboot.project.friendinfomgntsystem.exception.PersonNotFoundException;
+import com.springboot.project.friendinfomgntsystem.exception.RenameNotPermittedException;
 import com.springboot.project.friendinfomgntsystem.repository.PersonRepository;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
@@ -79,7 +81,7 @@ class PersonServiceTest {
         when(personRepository.findById(1L))
                 .thenReturn(Optional.empty());
 
-        assertThrows(RuntimeException.class, () -> personService.modify(1L, mockPersonDto()));
+        assertThrows(PersonNotFoundException.class, () -> personService.modify(1L, mockPersonDto()));
     }
 
     @Test
@@ -87,7 +89,7 @@ class PersonServiceTest {
         when(personRepository.findById(1L))
                 .thenReturn(Optional.of(new Person("tony")));
 
-        assertThrows(RuntimeException.class, () -> personService.modify(1L, mockPersonDto()));
+        assertThrows(RenameNotPermittedException.class, () -> personService.modify(1L, mockPersonDto()));
     }
 
     @Test
@@ -106,7 +108,7 @@ class PersonServiceTest {
         when(personRepository.findById(1L))
                 .thenReturn(Optional.empty());
 
-        assertThrows(RuntimeException.class, () -> personService.modify(1L, "daniel"));
+        assertThrows(PersonNotFoundException.class, () -> personService.modify(1L, "daniel"));
     }
 
     @Test
@@ -124,7 +126,7 @@ class PersonServiceTest {
         when(personRepository.findById(1L))
                 .thenReturn(Optional.empty());
 
-        assertThrows(RuntimeException.class, () -> personService.delete(1L));
+        assertThrows(PersonNotFoundException.class, () -> personService.delete(1L));
     }
 
     @Test
